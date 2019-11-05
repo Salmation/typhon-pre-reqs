943b4e2186f0d6604b4eb8f76d1d56fc10bd978b
Salmation <Salmation@users.noreply.github.com>
2019-11-05 15:56:06 +0000

    Update and rename main.sh to Step-1.sh

--- Step-1.sh
+++ Step-1.sh
@@ -81,29 +81,4 @@ mkdir /home/ducc/ducc_runtime
 cd /home/ducc/ && tar xzf /home/ducc/uima-ducc-3.0.0-bin.tar.gz && mv apache-uima-ducc-3.0.0/* /home/ducc/ducc_runtime/
 rm -Rf /home/ducc/apache-uima-ducc-3.0.0/
 cd /home/ducc/ducc_runtime/admin/ && /home/ducc/ducc_runtime/admin/ducc_post_install
-chown ducc.ducc -Rf /home/ducc/
-chmod 700 /home/ducc/ducc_runtime/admin/
-
-# Create res folder to store the results
-mkdir /tmp/res
-chown ducc.ducc -Rf /tmp/res/
-# Run check_ducc to check if UIMA is properly installed
-export LOGNAME="ducc"
-su - ducc -c "/home/ducc/ducc_runtime/admin/check_ducc"
-EOF
-
-# Install NFS (Filesystem Sharing)
-apt-get update
-apt install nfs-kernel-server
-
-# Update list of nodes to share the folder with and restart service
-echo "/home/ducc/ducc_runtime 1.1.1.1(rw,sync,no_subtree_check)" >> /etc/exports
-exportfs –a
-systemctl restart nfs-kernel-server
-
-# Add Worker Node IP in hosts
-# nano /etc/hosts
-sudo -s <<EOF
-#Start DUCC
-su - ducc -c "/home/ducc/ducc_runtime/admin/start_ducc" 
 EOF
